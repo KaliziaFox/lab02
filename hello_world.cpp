f43c1befc1ddf4f6076b3c8327357b2816a760fe
KaliziaFox <Kalugina-007@yandex.ru>
2023-03-09 19:14:43 +0300

    changed hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,10 @@
- #include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-        cout<<"Hello, World"<<endl;
+        cout<<"Input your name:";
+        string name;
+        cin>>name;
+        cout<<"Hello world from "<<name<<endl;
         return 0;
 }
